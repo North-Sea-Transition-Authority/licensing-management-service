141aac62191c969aedd50dae152097bc00014628
dbetts <danny.betts@fivium.co.uk>
2026-01-08 15:25:44 +0000

    Enhance LicenceStatus to include applicable licence types

--- src/main/java/uk/co/nstauthority/licensingmanagementservice/licence/LicenceStatus.java
+++ src/main/java/uk/co/nstauthority/licensingmanagementservice/licence/LicenceStatus.java
@@ -1,19 +1,34 @@
 package uk.co.nstauthority.licensingmanagementservice.licence;
 
+import java.util.Arrays;
+import java.util.List;
+
 public enum LicenceStatus {
-  EXTANT("Extant"),
-  REVOKED("Revoked"),
-  SURRENDERED("Surrendered"),
-  EXPIRED("Expired"),
-  SPLIT_AND_TERMINATED("Split and terminated");
+  EXTANT("Extant", Arrays.asList(LicenceType.values())),
+  REVOKED("Revoked", Arrays.asList(LicenceType.values())),
+  SURRENDERED("Surrendered", Arrays.asList(LicenceType.values())),
+  EXPIRED("Expired", Arrays.asList(LicenceType.values())),
+  SPLIT_AND_TERMINATED("Split and terminated", List.of(LicenceType.CARBON_STORAGE));
 
   private final String displayText;
+  private final List<LicenceType> applicableLicenceTypes;
 
-  LicenceStatus(String displayText) {
+  LicenceStatus(String displayText, List<LicenceType> applicableLicenceTypes) {
     this.displayText = displayText;
+    this.applicableLicenceTypes = applicableLicenceTypes;
   }
 
   public String getDisplayText() {
     return displayText;
+  }
+
+  public List<LicenceType> getApplicableLicenceTypes() {
+    return applicableLicenceTypes;
+  }
+
+  public static List<LicenceStatus> getApplicableStatusesForLicenceType(LicenceType licenceType) {
+    return Arrays.stream(LicenceStatus.values())
+        .filter(status -> status.getApplicableLicenceTypes().contains(licenceType))
+        .toList();
   }
 }

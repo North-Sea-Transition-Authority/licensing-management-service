0a168e1e0698179a51f3fb5e0d95bfb814335181
George O <115717712+GeorgeOG34@users.noreply.github.com>
2026-05-11 13:00:27 +0100

    EPGF-143: FindAllByAttribute Integration test. (#326)

---
+++ gis-framework/src/test/java/uk/co/fivium/gisframework/feature/FeatureRepositoryIntegrationTest.java
@@ -0,0 +1,70 @@
+package uk.co.fivium.gisframework.feature;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.test.context.ActiveProfiles;
+
+@DataJpaTest
+@ActiveProfiles("integration-test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class FeatureRepositoryIntegrationTest {
+
+  @Autowired
+  private FeatureRepository featureRepository;
+
+  @Test
+  void findAllByAttribute() {
+    var feature1 = FeatureTestUtil.newBuilder()
+        .withId(null)
+        .withFeatureName("feature1")
+        .withLegacyId(1)
+        .withAttributes(Map.of(
+            "some_attribute_1", "some_value_1"
+        ))
+        .build();
+    var feature2 = FeatureTestUtil.newBuilder()
+        .withId(null)
+        .withFeatureName("feature2")
+        .withLegacyId(2)
+        .withAttributes(Map.of(
+            "some_attribute_1", "some_value_2"
+        ))
+        .build();
+    var feature3 = FeatureTestUtil.newBuilder()
+        .withId(null)
+        .withFeatureName("feature3")
+        .withLegacyId(3)
+        .withAttributes(Map.of(
+            "some_attribute_2", "some_value_1"
+        ))
+        .build();
+
+    featureRepository.saveAll(
+        List.of(feature1, feature2, feature3)
+    );
+
+    var result = featureRepository.findAllByAttribute("some_attribute_1", "some_value_1");
+    var expectedResult = featureRepository.findById(feature1.getId()).orElseThrow();
+
+    assertThat(result)
+        .usingRecursiveComparison()
+        .isEqualTo(List.of(expectedResult));
+  }
+
+  @SpringBootConfiguration
+  @EnableAutoConfiguration
+  @EntityScan(basePackageClasses = Feature.class)
+  @EnableJpaRepositories(basePackageClasses = FeatureRepository.class)
+  static class TestApplication {
+  }
+}

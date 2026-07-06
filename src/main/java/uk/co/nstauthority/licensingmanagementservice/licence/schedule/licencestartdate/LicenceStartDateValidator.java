afe89073158f67bc7977b5d09127c44a9ecbebdc
Lauren <34923968+Lauren-M-Fivium@users.noreply.github.com>
2026-07-06 14:40:31 +0100

    LMS1-576: add minimum date to licence start date (#446)

--- src/main/java/uk/co/nstauthority/licensingmanagementservice/licence/schedule/licencestartdate/LicenceStartDateValidator.java
+++ src/main/java/uk/co/nstauthority/licensingmanagementservice/licence/schedule/licencestartdate/LicenceStartDateValidator.java
@@ -1,5 +1,7 @@
 package uk.co.nstauthority.licensingmanagementservice.licence.schedule.licencestartdate;
 
+import java.time.LocalDate;
+import java.time.Month;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.Errors;
 import uk.co.fivium.formlibrary.validator.date.ThreeFieldDateInputValidator;
@@ -10,6 +12,8 @@ public class LicenceStartDateValidator {
   boolean isValid(LicenceStartDateForm form, Errors errors) {
     ThreeFieldDateInputValidator.builder()
         .emptyInputErrorMessage("Provide the licence start date")
+        .mustBeAfterDate(LocalDate.of(1900, Month.JANUARY, 1))
+        .mustBeAfterDateErrorMessage("The date must be after 1 January 1900")
         .validate(form.getLicenceStartDate(), errors);
 
     return !errors.hasErrors();
--- src/test/java/uk/co/nstauthority/licensingmanagementservice/licence/schedule/licencestartdate/LicenceStartDateValidatorTest.java
+++ src/test/java/uk/co/nstauthority/licensingmanagementservice/licence/schedule/licencestartdate/LicenceStartDateValidatorTest.java
@@ -2,6 +2,7 @@ package uk.co.nstauthority.licensingmanagementservice.licence.schedule.licencest
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.Set;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -33,5 +34,33 @@ class LicenceStartDateValidatorTest {
     var bindingResult = ValidatorTestingUtil.getBindingResult(form);
 
     assertThat(licenceStartDateValidator.isValid(form, bindingResult)).isFalse();
+  }
+
+  @Test
+  void isValid_whenDateIsBeforeMinimumYear_thenReturnFalse() {
+    var form = new LicenceStartDateForm();
+    form.getLicenceStartDate().setDay(1);
+    form.getLicenceStartDate().setMonth(1);
+    form.getLicenceStartDate().setYear(1899);
+
+    var bindingResult = ValidatorTestingUtil.getBindingResult(form);
+
+    assertThat(licenceStartDateValidator.isValid(form, bindingResult)).isFalse();
+    assertThat(ValidatorTestingUtil.extractErrorMessages(bindingResult))
+        .containsEntry("licenceStartDate.dayInput.inputValue", Set.of("The date must be after 1 January 1900"));
+  }
+
+  @Test
+  void isValid_whenDateIsMinimumYear_thenReturnFalse() {
+    var form = new LicenceStartDateForm();
+    form.getLicenceStartDate().setDay(1);
+    form.getLicenceStartDate().setMonth(1);
+    form.getLicenceStartDate().setYear(1900);
+
+    var bindingResult = ValidatorTestingUtil.getBindingResult(form);
+
+    assertThat(licenceStartDateValidator.isValid(form, bindingResult)).isFalse();
+    assertThat(ValidatorTestingUtil.extractErrorMessages(bindingResult))
+        .containsEntry("licenceStartDate.dayInput.inputValue", Set.of("The date must be after 1 January 1900"));
   }
 }
